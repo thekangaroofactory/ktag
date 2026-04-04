1e5b4e59826bd07f0d80a8f1f7af03b25a6ed30e
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-04-04 12:55:38 +0200

    Fixed #12

--- R/ktag.R
+++ R/ktag.R
@@ -13,6 +13,9 @@
 #' - what: the name of the action. Usually the id of the input, including its namespace
 #' - how: additional info to log, like the value of an input for example
 #'
+#' When an html component has no id, the function will raise a warning in the console
+#' and ignore the call.
+#'
 #' @export
 #'
 #' @importFrom utils head
@@ -26,12 +29,18 @@
 
 ktag <- function(..., path = Sys.getenv("KTAG_PATH")){
 
+  # -- check env
   stopifnot("KTAG_PATH environment variable is not set or path argument is empty" = !identical(path, ""))
-
   if(Sys.getenv("KTAG_STATE") == "OFF")
     return()
 
+  # -- args
   arg_list <- list(...)
+  if(identical(arg_list$what, "")){
+    warning("Missing id (what) on a component with class ktag - event is ignored.")
+    return()}
+
+  # -- prepare & log
   x <- data.frame(who = arg_list$who,
                   when = if("when" %in% names(arg_list)) arg_list$when else round(as.numeric(Sys.time()) * 1000, digits = 0),
                   where = if("where" %in% names(arg_list)) arg_list$where else paste(utils::head(unlist(strsplit(arg_list$what, split = "-")), -1L), collapse = "-"),
