3b8b4b32e9252723ef1b0000fa30e429b439bf22
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-04-04 19:03:39 +0200

    Fixed #15

--- R/ktag.R
+++ R/ktag.R
@@ -35,7 +35,7 @@
 ktag <- function(..., path = Sys.getenv("KTAG_PATH")){
 
   # -- check env
-  stopifnot("KTAG_PATH environment variable is not set or path argument is empty" = !identical(path, ""))
+  stopifnot("KTAG_PATH environment variable is not set or path argument is empty." = !identical(path, ""))
   if(Sys.getenv("KTAG_STATE") == "OFF")
     return()
 
@@ -43,22 +43,27 @@ ktag <- function(..., path = Sys.getenv("KTAG_PATH")){
   arg_list <- list(...)
 
   if(!length(arg_list)){
-    warning("At least what should be passed to ... argument — call is ignored.")
+    warning("At least what should be passed to ... argument - call is ignored.")
     return()}
 
   if(identical(arg_list$what, "") || is.null(arg_list$what)){
-    warning("What is either NULL or empty. Check for missing id on a component with class ktag — event is ignored.")
+    warning("What is either NULL or empty. Check for missing id on a component with class ktag - event is ignored.")
     return()}
 
-  # -- prepare & log
-  x <- data.frame(who = if("when" %in% names(arg_list)) arg_list$who else shiny::getDefaultReactiveDomain()$token,
-                  when = if("when" %in% names(arg_list)) arg_list$when else round(as.numeric(Sys.time()) * 1000, digits = 0),
-                  where = if("where" %in% names(arg_list)) arg_list$where else paste(utils::head(unlist(strsplit(arg_list$what, split = "-")), -1L), collapse = "-"),
-                  what = utils::tail(unlist(strsplit(arg_list$what, split = "-")), 1L),
-                  how = if("how" %in% names(arg_list)) arg_list$how else NA)
+  # -- prepare values
+  if(!"who" %in% names(arg_list))
+    arg_list$who <- ifelse(shiny::isRunning(), shiny::getDefaultReactiveDomain()$token, "unknown")
+  if(!"when" %in% names(arg_list))
+    arg_list$when <- round(as.numeric(Sys.time()) * 1000, digits = 0)
+  if(!"where" %in% names(arg_list))
+    arg_list$where <- paste(utils::head(unlist(strsplit(arg_list$what, split = "-")), -1L), collapse = "-")
+  arg_list$where <- utils::tail(unlist(strsplit(arg_list$what, split = "-")), 1L)
+  if(!"how" %in% names(arg_list))
+    arg_list$how <- NA
 
+  # -- write
+  x <- as.data.frame(arg_list)
   f_exists <- !file.exists(file.path(path, "ktag.csv"))
-
   utils::write.table(x,
                      file = file.path(path, "ktag.csv"),
                      append = !f_exists,
